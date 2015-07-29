f68289e19cc46b908b2a577c1a0ba17d2c9fa433
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-29 18:30:19 +0200

    New version

--- postInstall.sh
+++ postInstall.sh
@@ -1,12 +1,20 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 NODE_MODULES="$DIR/node_modules"
 PARENT_NODE_MODULES="$DIR/../../node_modules"
+
 # Check if module is included as a dependency
 if [ -d "$PARENT_NODE_MODULES" ]; then
-  # We are a dependency ! Let's symbolic link
-  ln -s "$NODE_MODULES/babel-eslint" "$PARENT_NODE_MODULES/babel-eslint"
-  ln -s "$NODE_MODULES/eslint" "$PARENT_NODE_MODULES/eslint"
-  ln -s "$NODE_MODULES/eslint-plugin-filenames" "$PARENT_NODE_MODULES/eslint-plugin-filenames"
-  ln -s "$NODE_MODULES/eslint-plugin-react" "$PARENT_NODE_MODULES/eslint-plugin-react"
-  ln -s "$NODE_MODULES/eslint-plugin-require-jsdoc" "$PARENT_NODE_MODULES/eslint-plugin-require-jsdoc"
+  # We are a dependency ! Let's copy
+  rm -rf "$NODE_MODULES/babel-eslint"
+  rm -rf "$NODE_MODULES/eslint"
+  rm -rf "$NODE_MODULES/eslint-plugin-filenames"
+  rm -rf "$NODE_MODULES/eslint-plugin-react"
+  rm -rf "$NODE_MODULES/eslint-plugin-require-jsdoc"
+  cp -r "$NODE_MODULES/babel-eslint" "$PARENT_NODE_MODULES/babel-eslint"
+  cp -r "$NODE_MODULES/eslint" "$PARENT_NODE_MODULES/eslint"
+  cp -r "$NODE_MODULES/eslint-plugin-filenames" "$PARENT_NODE_MODULES/eslint-plugin-filenames"
+  cp -r "$NODE_MODULES/eslint-plugin-react" "$PARENT_NODE_MODULES/eslint-plugin-react"
+  cp -r "$NODE_MODULES/eslint-plugin-require-jsdoc" "$PARENT_NODE_MODULES/eslint-plugin-require-jsdoc"
+  cp -f "$NODE_MODULES/.bin/eslint" "$PARENT_NODE_MODULES/.bin/"
+  cp -f "$NODE_MODULES/.bin/eslint.cmd" "$PARENT_NODE_MODULES/.bin/"
 fi
